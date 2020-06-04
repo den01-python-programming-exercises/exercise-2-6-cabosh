37414075f56e5c04904dfcbd52c45bd18156f033
cabosh <callum.bosher@gmail.com>
2020-06-04 22:26:46 +0100

    Attempted commit

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,12 @@
 def main():
     #write your code below this line
 
+    while True:
+
+        number = int(input("Enter a number: "))
+
+        if number == 4:
+            break
+
 if __name__ == '__main__':
     main()
